8b068c92d93c0a764307c3a5460ed81b5395f008
Subhash Diwakar <subhashd@atulsia.com>
2019-12-11 14:11:35 +0530

    bookcontroller updated

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -10,8 +10,8 @@ class BooksController extends Controller
 {
     private function getBookQueryBackup(Request $request)
     {
-        $query = Book::/* orderBy('download_count', 'desc')
-                    -> */with(['authors', 'shelves', 'subjects', 'languages', 'formats']);
+        $query = Book::orderBy('download_count', 'desc')
+                    ->with(['authors', 'shelves', 'subjects', 'languages', 'formats']);
         
         if($request->has('book_id'))
             $query->whereIn('gutenberg_id', $request->book_id);
@@ -31,14 +31,28 @@ class BooksController extends Controller
         if($request->has('topic')) {
             // $query->where(function($q) use ($request) {
                 $query->orWhereHas('shelves', function($qt) use ($request) {
-                    foreach($request->topic as $topic)
-                    {
-                        $qt->orWhere('name', 'LIKE', '%'.$topic);     
+                    if(is_array($request->topic)) {
+                        foreach($request->topic as $tp_key => $topic)
+                        {
+                            if($tp_key === 0)
+                                $qt->where('name', 'LIKE', '%'.$topic);     
+                            else
+                                $qt->orWhere('name', 'LIKE', '%'.$topic);     
+                        }
+                    } else {
+                        $qt->where('name', 'LIKE', '%'.$request->topic);
                     }
                 })->orWhereHas('subjects', function($qt) use ($request) {
-                    foreach($request->topic as $topic)
-                    {
-                        $qt->orWhere('name', 'LIKE', '%'.$topic);     
+                    if(is_array($request->topic)) {
+                        foreach($request->topic as $tp_key => $topic)
+                        {
+                            if($tp_key === 0)
+                                $qt->where('name', 'LIKE', '%'.$topic);     
+                            else
+                                $qt->orWhere('name', 'LIKE', '%'.$topic);     
+                        }
+                    } else {
+                        $qt->where('name', 'LIKE', '%'.$request->topic);
                     }
                 });
             // });
@@ -46,85 +60,40 @@ class BooksController extends Controller
         
         if($request->has('author')) {
             $query->whereHas('authors', function($q) use ($request) {
-                foreach($request->author as $author)
-                {
-                    $q->orWhere('name', 'LIKE', '%'.$author);     
+                if(is_array($request->author)) {
+                    foreach($request->author as $a_key => $author)
+                    {
+                        if($a_key === 0)
+                            $q->where('name', 'LIKE', '%'.$author);     
+                        else
+                            $q->orWhere('name', 'LIKE', '%'.$author);     
+                    }
+                } else {
+                    $q->where('name', 'LIKE', '%'.$request->author);
                 }
             });
         }
         
         if($request->has('title')) {
-            foreach($request->title as $title)
-            {
-                $query->orWhere('title', 'LIKE', '%'.$title);     
-            }
-        }
-
-        return $query;
-    }
-    
-    private function getBookQuery(Request $request)
-    {
-        $query = Book::select("books_book.*", 
-                            DB::raw("(select books_author.birth_year", "books_author.death_year","books_author.name as author_name) as authors"), 
-                            DB::raw("(select books_bookshelf.name as book_shelf) as shevles"), 
-                            DB::raw("(select books_subject.name as book_subject) as subjects"), 
-                            DB::raw("(select books_language.code as book_language) as language"), 
-                            DB::raw("(select books_format.mime_type", "books_format.url) as formats") 
-                        )
-            ->join("books_book_authors", "books_book.id", "=", "books_book_authors.book_id")
-            ->join("books_author", "books_author.id", "=", "books_book_authors.author_id")
-            ->join("books_book_bookshelves", "books_book.id", "=", "books_book_bookshelves.book_id")
-            ->join("books_bookshelf", "books_bookshelf.id", "=", "books_book_bookshelves.bookshelf_id")
-            ->join("books_book_subjects", "books_book.id", "=", "books_book_subjects.book_id")
-            ->join("books_subject", "books_subject.id", "=", "books_book_subjects.subject_id")
-            ->join("books_book_languages", "books_book.id", "=", "books_book_languages.book_id")
-            ->join("books_language", "books_language.id", "=", "books_book_languages.language_id")
-            ->join("books_format",function($join){
-                $join->on("books_format.book_id","=","books_book.id");
-            });
-        
-        if($request->has('book_id'))
-            $query->whereIn('books_book.gutenberg_id', $request->book_id);
-        
-        if($request->has('language'))
-            $query->whereIn('books_language.code', $request->language);
-        
-        if($request->has('mime_type'))
-            $query->whereIn('books_format.mime_type', $request->mime_type);
-
-        if($request->has('topic')) {
-            $query->where(function($q) use ($request) {
-                foreach($request->topic as $topic)
+            if(is_array($request->title)) {
+                foreach($request->title as $t_key => $title)
                 {
-                    $q->orWhere('books_subject.name', 'LIKE', '%'.$topic.'%')
-                    ->orWhere('books_bookshelf.name', 'LIKE', '%'.$topic.'%');
+                    if($t_key === 0)
+                        $query->where('title', 'LIKE', '%'.$title);     
+                    else
+                        $query->orWhere('title', 'LIKE', '%'.$title);     
                 }
-            });
-        }
-        
-        if($request->has('author')) {
-            foreach($request->author as $author)
-            {
-                $query->orWhere('books_author.name', 'LIKE', '%'.$author);     
-            }
-        }
-        
-        if($request->has('title')) {
-            foreach($request->title as $title)
-            {
-                $query->orWhere('books_book.title', 'LIKE', '%'.$title);     
+            } else {
+                $query->where('title', 'LIKE', '%'.$request->title); 
             }
         }
 
-        $query->orderBy('books_book.download_count', 'DESC');
-        // $query->groupBy('books_book.id');
         return $query;
     }
-
+ 
     public function filter(Request $request) 
     {
-        $query = $this->getBookQuery($request);
+        $query = $this->getBookQueryBackup($request);
         $totalCount = $query->count();
 
         if($request->has('limit'))
@@ -134,6 +103,6 @@ class BooksController extends Controller
             $query->offset($request->offset);
         
         $results = $query->get();
-        return response()->json(['results' => $results, 'totalCount' => $totalCount]);
+        return response()->json(['totalCount' => $totalCount, 'results' => $results]);
     }
 }
